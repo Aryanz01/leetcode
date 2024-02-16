aa92e5da0b4f3330eaa18471fbe0139e33ce947f
Aryanz01 <136065084+Aryanz01@users.noreply.github.com>
2024-02-16 19:13:59 +0530

    Time: 27 ms (86.12%), Space: 33.1 MB (69.89%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -1,40 +1,3 @@
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int val;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//  * };
-//  */
-// class Solution {
-// public:
-//     TreeNode* searchBST(TreeNode* root, int val) {
-//         if(root==NULL){
-//             return NULL;
-//         }
-           
-//            if(root->val==val){
-//                return root;
-//            }
-           
-//            if(root->val>val){
-//                   // root=root->left;
-//                    return searchBST(root->left,val);
-//                }
-               
-//                else{
-//                   // root=root->right;
-//                    return searchBST(root->left,val);
-               
-//            }
-//         return NULL;
-//     }
-// };
-
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -49,15 +12,51 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(root == nullptr) return nullptr;
-        if(root->val == val) return root;
-        if(val < root->val) {
-            return searchBST(root->left, val);
-        } else {
-            return searchBST(root->right, val);
+        if(root==nullptr){
+            return nullptr;
         }
+           
+           if(root->val==val){
+               return root;
+           }
+           
+           if(root->val>val){
+                  
+                   return searchBST(root->left,val);
+               }
+               
+               else{
+                  
+                   return searchBST(root->right,val);
+               
+           }
     }
 };
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode* searchBST(TreeNode* root, int val) {
+//         if(root == nullptr) return nullptr;
+//         if(root->val == val) return root;
+//         if(val < root->val) {
+//             return searchBST(root->left, val);
+//         } else {
+//             return searchBST(root->right, val);
+//         }
+//     }
+// };
 
 
 
