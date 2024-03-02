5456a184d79daa3495702486dd8684e18b8a695e
Aryanz01 <136065084+Aryanz01@users.noreply.github.com>
2024-03-02 18:40:36 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (49.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,6 +1,32 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//         vector<int> dp(n+1,-1);
+//         return ans(n,dp);
+//     }
+    
+//     int ans(int n, vector<int> dp){
+//         if(n==0){
+//             return 0;
+//         }
+//         if(n==1){
+//             return 1;
+//         }
+        
+//         if(dp[n] !=-1){
+//             return dp[n];
+//         }
+        
+//         dp[n]=ans(n-1 , dp) +ans(n-2 , dp);
+//         return dp[n];
+//     }
+// };
+
 class Solution {
 public:
     int fib(int n) {
+        int dp[n+1];
+        
         if(n==0){
             return 0;
         }
@@ -8,7 +34,15 @@ public:
             return 1;
         }
         
-        return fib(n-1) +fib(n-2);
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+        
         
     }
 };
