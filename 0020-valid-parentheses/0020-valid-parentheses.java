1ac84be7fc43f0241354fee43f84383d4e1e9658
Aryanz01 <136065084+Aryanz01@users.noreply.github.com>
2024-07-10 02:34:34 +0530

    Time: 1 ms (98.97%), Space: 41.2 MB (71.33%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false; // mismatch or extra closing bracket
+            }
+        }
+        
+        return stack.isEmpty(); // stack should be empty for valid string
+    }
+}
