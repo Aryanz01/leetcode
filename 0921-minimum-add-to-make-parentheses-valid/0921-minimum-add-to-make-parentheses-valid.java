40e1c55d4ca015e2db29c55bbe3848469d1cace4
Aryanz01 <136065084+Aryanz01@users.noreply.github.com>
2024-10-10 01:18:50 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (79.22%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,24 +1,48 @@
-import java.util.Stack;
+// import java.util.Stack;
+
+// class Solution {
+//     public int minAddToMakeValid(String s) {
+//         int x = 0;  // To count unmatched ')'
+//         Stack<Character> st = new Stack<Character>();
+
+//         // Iterate through the string
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == '(') {
+//                 st.push(s.charAt(i));  // Push unmatched '('
+//             } else if (s.charAt(i) == ')') {
+//                 if (!st.isEmpty()) {
+//                     st.pop();  // Match ')' with '('
+//                 } else {
+//                     x++;  // Unmatched ')'
+//                 }
+//             }
+//         }
+        
+//         // st.size() will have unmatched '(' and x will have unmatched ')'
+//         return st.size() + x;
+//     }
+// }
+
+
 
 class Solution {
     public int minAddToMakeValid(String s) {
-        int x = 0;  // To count unmatched ')'
-        Stack<Character> st = new Stack<Character>();
+        int open = 0, close = 0;
 
-        // Iterate through the string
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                st.push(s.charAt(i));  // Push unmatched '('
-            } else if (s.charAt(i) == ')') {
-                if (!st.isEmpty()) {
-                    st.pop();  // Match ')' with '('
+                open++;
+            } else {
+                if (open > 0) {
+                    open--;  // Match a '(' with a ')'
                 } else {
-                    x++;  // Unmatched ')'
+                    close++;  // Unmatched ')'
                 }
             }
         }
-        
-        // st.size() will have unmatched '(' and x will have unmatched ')'
-        return st.size() + x;
+
+        // The result is the sum of unmatched '(' and ')'
+        return open + close;
     }
 }
+
