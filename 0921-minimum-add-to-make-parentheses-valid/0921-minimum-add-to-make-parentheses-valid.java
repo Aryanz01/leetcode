2405d366bb4d1bfe52d6d32263b9ce5c9de3ae0b
Aryanz01 <136065084+Aryanz01@users.noreply.github.com>
2024-10-10 01:19:05 +0530

    Time: 1 ms (58.01%), Space: 41.3 MB (79.22%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,48 +1,48 @@
-// import java.util.Stack;
-
-// class Solution {
-//     public int minAddToMakeValid(String s) {
-//         int x = 0;  // To count unmatched ')'
-//         Stack<Character> st = new Stack<Character>();
-
-//         // Iterate through the string
-//         for (int i = 0; i < s.length(); i++) {
-//             if (s.charAt(i) == '(') {
-//                 st.push(s.charAt(i));  // Push unmatched '('
-//             } else if (s.charAt(i) == ')') {
-//                 if (!st.isEmpty()) {
-//                     st.pop();  // Match ')' with '('
-//                 } else {
-//                     x++;  // Unmatched ')'
-//                 }
-//             }
-//         }
-        
-//         // st.size() will have unmatched '(' and x will have unmatched ')'
-//         return st.size() + x;
-//     }
-// }
-
-
+import java.util.Stack;
 
 class Solution {
     public int minAddToMakeValid(String s) {
-        int open = 0, close = 0;
+        int x = 0;  // To count unmatched ')'
+        Stack<Character> st = new Stack<Character>();
 
+        // Iterate through the string
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                open++;
-            } else {
-                if (open > 0) {
-                    open--;  // Match a '(' with a ')'
+                st.push(s.charAt(i));  // Push unmatched '('
+            } else if (s.charAt(i) == ')') {
+                if (!st.isEmpty()) {
+                    st.pop();  // Match ')' with '('
                 } else {
-                    close++;  // Unmatched ')'
+                    x++;  // Unmatched ')'
                 }
             }
         }
-
-        // The result is the sum of unmatched '(' and ')'
-        return open + close;
+        
+        // st.size() will have unmatched '(' and x will have unmatched ')'
+        return st.size() + x;
     }
 }
+
+
+
+// class Solution {
+//     public int minAddToMakeValid(String s) {
+//         int open = 0, close = 0;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == '(') {
+//                 open++;
+//             } else {
+//                 if (open > 0) {
+//                     open--;  // Match a '(' with a ')'
+//                 } else {
+//                     close++;  // Unmatched ')'
+//                 }
+//             }
+//         }
+
+//         // The result is the sum of unmatched '(' and ')'
+//         return open + close;
+//     }
+// }
 
