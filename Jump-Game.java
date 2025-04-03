2e730e7a1843f90bdcadc54933ecb2812c87d16e
Aryanz01 <136065084+Aryanz01@users.noreply.github.com>
2025-04-04 02:12:39 +0530

    [55] [Time Beats: 42.70%] [Memory Beats: 42.35%] - LeetPush

--- Jump-Game.java
+++ Jump-Game.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean canJump(int[] nums) {
         int max_index=0;
             for(int i=0; i<nums.length; i++){
+                
             if(i>max_index){
                 return false;
             }
